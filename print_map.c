b524001d4f99d8d43c9cf0cab00f7da4db8e0e53
tom billard <loulechatdu44@hotmail.fr>
2016-10-10 05:10:24 +0200

    fix map printing

--- print_map.c
+++ print_map.c
@@ -4,33 +4,19 @@ void        print_map(int **map, int size)
 {
     int x;
     int y;
-    int i;
 
-    i = 0;
     x = 0;
     y = 0;
-    ft_putstr("\\ ");
-    while (i < size)
-        ft_putnbr(i++);
-    ft_putendl("");
-    i = 0;
-    ft_putstr("  ");
-    while (i++ < size)
-        ft_putchar('-');
-    i = 0;
-    ft_putendl("");
     while (y < size)
     {
-        ft_putnbr(i);
-        ft_putchar('|');
-        i++;
         while (x < size)
         {
-            if (map[y][x] & USE_SET)
+            if (map[y][x] & P_TWO_SET || map[y][x] & P_ONE_SET)
+                ft_putnbr(2);
+            else if (map[y][x] & USE_SET)
                 ft_putnbr(1);
             else
                 ft_putnbr(0);
-            //ft_putnbr(map[y][x]);
             x++;
         }
         x = 0;
