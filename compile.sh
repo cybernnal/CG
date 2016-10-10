77f3ad8657b4228a23d01bb990bf3e5153d7e32e
tbillard <tbillard@student.42.fr>
2016-10-10 03:29:31 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 make -C libft/
-gcc -o col_game *.c -I./ -I./libft/ libft/libft.a -I/usr/include/SDL2 -D_REENTRANT -L/usr/lib/x86_64-linux-gnu -lSDL2 -lm
+gcc -o col_game *.c -I./ -I./libft/ libft/libft.a 'sdl2-config --cflags' 'sdl2-config --libs' -lm
